2a52cf83e86a6b8da2840c91f221b557c4372049
ATimpe <atimpe@csumb.edu>
2021-09-07 15:20:54 -0700

    Added MVC for game logic

---
+++ Assets/Scripts/GameModel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModel : GameElement
+{
+    // GameModel keeps the object data. Add variables for the game to keep track of
+    public int gold;
+    public int timeRemaining;
+    // TODO: add counters for seperate tiles once we come up with differnt kinds
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
